132a02e1eccd00eb8a39e5d2549a2103e94e7112
Pablo Ledesma <Pablo Ledesma>
2016-03-16 00:30:33 -0500

    add redis -2:17

--- app.js
+++ app.js
@@ -3,8 +3,20 @@ var app = express();
 var server = require('http').createServer(app);
 var io = require('socket.io')(server);
 var messages = [];
+var redis = require('redis');
+var redisClient = redis.createClient();
 
 var storeMessage = function(name, data){
+
+	//Need to turn object into string to store in redis
+	var message = JSON.stringify({ name: name, data: data });
+
+	redisClient.lpush("messages", message, function(err, response){
+
+		//Keeps newest 10 items
+		redisClient.ltrim("messages", 0, 9);
+
+	});
 
 	//Add the messange to end of array
 	messages.push({ name: name, data: data });
@@ -20,13 +32,26 @@ io.on('connection', function(client){
 	
 	client.on('join', function(name){
 		
+		
+		redisClient.lrange("messages", 0, -1, function(err, messages){
+
+			//reverse so they are emitted in correct order
+			messages = messages.reverse();
+
+		});
+
 		//Send the nickname associated with the client
 		client.nickname = name;
 		client.broadcast.emit("chat", name + " joined the chat");
 
 		//iterate through messages array and emit a message on the connecting client for each one
 		messages.forEach(function(message){
+
+			//parse into json object
+			message = JSON.parse(message);
+
 			client.emit("messages", message.name + ": " + message.data + "<br>");
+		
 		});
 	
 	});
