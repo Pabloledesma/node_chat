3d63b840036c5c801b9b5bacaa091ad4b19e96b5
Pablo Ledesma <Pablo Ledesma>
2016-03-15 23:49:56 -0500

    persisting messages

---
+++ app.js
@@ -0,0 +1,49 @@
+var express = require('express');
+var app = express();
+var server = require('http').createServer(app);
+var io = require('socket.io')(server);
+var messages = [];
+
+var storeMessage = function(name, data){
+
+	//Add the messange to end of array
+	messages.push({ name: name, data: data });
+
+	//if more than 10 messages long, remove the first one
+	if(messages.length > 10){
+		messages.shift();
+	}
+
+};
+
+io.on('connection', function(client){
+	
+	client.on('join', function(name){
+		
+		//Send the nickname associated with the client
+		client.nickname = name;
+		client.broadcast.emit("chat", name + " joined the chat");
+	
+	});
+
+	client.on('messages', function(message){ 
+
+		//Get the nickname of this client before broadcasting message
+		var nickname = client.nickname;
+
+		//Broadcacst with the name and message
+		client.broadcast.emit("messages", nickname + ": " + message + "<br>");
+
+		//Send the same message back to our client
+		client.emit("messages", nickname + ": " + message);
+
+		//When client sends a message call storeMessage
+		storeMessage(name, message);
+	});
+});
+
+app.get('/', function(req, res){
+	res.sendFile(__dirname + '/index.html');
+});
+
+server.listen(8080);
